a16c57395e3f93a3a9db8664685c08d543a5e919
IvanTsve <iv_plamenovv@abv.bg>
2026-05-25 17:01:41 +0300

    add texture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,28 @@
+import {
+    Assets,
+    Texture,
+    Sprite,
+} from 'pixi.js';
+import {
+    useEffect,
+    useState,
+} from 'react';
+
+import {
+    extend,
+} from '@pixi/react';
+extend({ Sprite });
+
 export const Hero = () => {
-  return (
-    <></>
-  )
-}
+    const [heroTexture, setHeroTexture] = useState(Texture.EMPTY);
+
+    useEffect(() => {
+        Assets.load(import.meta.env.VITE_PIXI_HERO_URL).then((result) => {
+            setHeroTexture(result);
+        });
+    }, []);
+
+    return (
+        <pixiSprite texture={heroTexture} x={400} y={280} anchor={0.5} />
+    );
+};
